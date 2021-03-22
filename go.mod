5e91a7e3d0dcb44730674ee927a2524d54fe72cc
endingman <631925002@qq.com>
2021-03-22 14:15:46 +0800

    尝试 http.ServeMux

--- go.mod
+++ go.mod
@@ -7,6 +7,12 @@ require (
 	github.com/creack/pty v1.1.11 // indirect
 	github.com/fatih/color v1.10.0 // indirect
 	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/pelletier/go-toml v1.8.1 // indirect
+	github.com/stretchr/objx v0.3.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
 	golang.org/x/sys v0.0.0-20210320140829-1e4c9ba3b0c4 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
+
 /**
 http.HandleFunc 用以指定处理 HTTP 请求的函数，
 此函数允许我们只写一个 handler（在此例子中 handlerFunc，可任意命名），
@@ -11,22 +12,39 @@ http.HandleFunc 用以指定处理 HTTP 请求的函数，
 
 http.Request 是用户的请求信息，一般用 r 作为简写。
 http.ResponseWriter 是返回用户的响应，一般用 w 作为简写。
- */
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-     w.Header().Set("Content-Type", "text/html; charset=utf-8")
-     if r.URL.Path == "/" {
-        fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
-    } else if r.URL.Path == "/about" {
-        fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
-            "<a href=\"mailto:summer@example.com\">summer@example.com</a>")
-    } else {
-        w.WriteHeader(http.StatusNotFound)
-        fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
-            "<p>如有疑惑，请联系我们。</p>")
-    }
+*/
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Hello , 欢迎来到 goblog！</h1>")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
+			"<p>如有疑惑，请联系我们。</p>")
+	}
 }
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+		"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
+}
+
 func main() {
-    http.HandleFunc("/", handlerFunc)
-    //http.ListenAndServe 用以监听本地 3000 端口以提供服务，标准的 HTTP 端口是 80 端口
-    http.ListenAndServe(":3000", nil)
+	router := http.NewServeMux()
+
+	//方法过滤
+	router.HandleFunc("/articles", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET":
+			fmt.Fprint(w, "访问文章列表")
+		case "POST":
+			fmt.Fprint(w, "创建新的文章")
+		}
+	})
+
+	http.HandleFunc("/", defaultHandler)
+	http.HandleFunc("/about", aboutHandler)
+	//http.ListenAndServe 用以监听本地 3000 端口以提供服务，标准的 HTTP 端口是 80 端口
+	http.ListenAndServe(":3000", router)
 }
