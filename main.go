932ae87f56e5a2a187a26e5047f3070d8270fe94
endingman <631925002@qq.com>
2021-03-22 15:24:14 +0800

    新建博文表单

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"strings"
 )
 
+//包级别的变量声明时不能使用 := 语法，修改为带关键词 var 的变量声明
+var router = mux.NewRouter().StrictSlash(true)
+
 /**
 http.HandleFunc 用以指定处理 HTTP 请求的函数，
 此函数允许我们只写一个 handler（在此例子中 handlerFunc，可任意命名），
@@ -46,6 +49,26 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "创建新的文章")
 }
 
+func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
+	html := `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>创建文章 —— 我的技术博客</title>
+</head>
+<body>
+    <form action="%s" method="post">
+        <p><input type="text" name="title"></p>
+        <p><textarea name="body" cols="30" rows="10"></textarea></p>
+        <p><button type="submit">提交</button></p>
+    </form>
+</body>
+</html>
+`
+	storeURL, _ := router.Get("articles.store").URL()
+	fmt.Fprintf(w, html, storeURL)
+}
+
 func forceHTMLMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// 1. 设置标头
@@ -73,14 +96,13 @@ func main() {
 	//可以看到有 / 的链接会报 404 错误：
 	//Gorilla Mux 提供了一个 StrictSlash(value bool) 函数处理`/`问题
 
-	router := mux.NewRouter().StrictSlash(true)
-
 	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
 	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
 
 	router.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandler).Methods("GET").Name("articles.show")
 	router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
 	router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
+	router.HandleFunc("/articles/create", articlesCreateHandler).Methods("GET").Name("articles.create")
 
 	// 自定义 404 页面
 	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
@@ -89,12 +111,10 @@ func main() {
 	router.Use(forceHTMLMiddleware)
 
 	// 通过命名路由获取 URL 示例
-	homeURL, _ := router.Get("home").URL()
-	fmt.Println("homeURL: ", homeURL)
-	articleURL, _ := router.Get("articles.show").URL("id", "23")
-	fmt.Println("articleURL: ", articleURL)
-
-	http.ListenAndServe(":3000", router)
+	//homeURL, _ := router.Get("home").URL()
+	//fmt.Println("homeURL: ", homeURL)
+	//articleURL, _ := router.Get("articles.show").URL("id", "23")
+	//fmt.Println("articleURL: ", articleURL)
 
 	//http.ListenAndServe 用以监听本地 3000 端口以提供服务，标准的 HTTP 端口是 80 端口
 	http.ListenAndServe(":3000", removeTrailingSlash(router))
