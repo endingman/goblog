606db3ac5df1910044734db886806199d2071b2d
endingman <631925002@qq.com>
2021-03-22 15:10:41 +0800

    移除 URL 路径中的斜杆

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
+	"strings"
 )
 
 /**
@@ -15,69 +16,86 @@ http.Request 是用户的请求信息，一般用 r 作为简写。
 http.ResponseWriter 是返回用户的响应，一般用 w 作为简写。
 */
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "text/html; charset=utf-8")
-    fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog！</h1>")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog！</h1>")
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "text/html; charset=utf-8")
-    fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
-        "<a href=\"mailto:summer@example.com\">summer@example.com</a>")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+		"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
 }
 
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 
-    w.WriteHeader(http.StatusNotFound)
-    fmt.Fprint(w, "<h1>请求页面未找到 :(</h1><p>如有疑惑，请联系我们。</p>")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>请求页面未找到 :(</h1><p>如有疑惑，请联系我们。</p>")
 }
 
 func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    id := vars["id"]
-    fmt.Fprint(w, "文章 ID："+id)
+	vars := mux.Vars(r)
+	id := vars["id"]
+	fmt.Fprint(w, "文章 ID："+id)
 }
 
 func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "访问文章列表")
+	fmt.Fprint(w, "访问文章列表")
 }
 
 func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "创建新的文章")
+	fmt.Fprint(w, "创建新的文章")
 }
 
-func forceHTMLMiddleware(next http.Handler)  http.Handler {
+func forceHTMLMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// 1. 设置标头
-        w.Header().Set("Content-Type", "text/html; charset=utf-8")
-        // 2. 继续处理请求
-        next.ServeHTTP(w, r)
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		// 2. 继续处理请求
+		next.ServeHTTP(w, r)
+	})
+}
+
+func removeTrailingSlash(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			//strings 包提供的 TrimSuffix(s, suffix string) string 函数来移除 / 后缀
+			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
+		}
+		next.ServeHTTP(w, r)
 	})
 }
 
 func main() {
-	router := mux.NewRouter()//mux路由，gorilla/mux 因实现了 net/http 包的 http.Handler 接口，故兼容 http.ServeMux
+	//mux路由，gorilla/mux 因实现了 net/http 包的 http.Handler 接口，故兼容 http.ServeMux
+	//访问以下两个链接：
+	//localhost:3000/about
+	//localhost:3000/about/
+	//可以看到有 / 的链接会报 404 错误：
+	//Gorilla Mux 提供了一个 StrictSlash(value bool) 函数处理`/`问题
+
+	router := mux.NewRouter().StrictSlash(true)
 
 	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
-    router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
+	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
 
-    router.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandler).Methods("GET").Name("articles.show")
-    router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
-    router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
+	router.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandler).Methods("GET").Name("articles.show")
+	router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
+	router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
 
-    // 自定义 404 页面
-    router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+	// 自定义 404 页面
+	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
 
 	// 中间件：强制内容类型为 HTML
 	router.Use(forceHTMLMiddleware)
 
-    // 通过命名路由获取 URL 示例
-    homeURL, _ := router.Get("home").URL()
-    fmt.Println("homeURL: ", homeURL)
-    articleURL, _ := router.Get("articles.show").URL("id", "23")
-    fmt.Println("articleURL: ", articleURL)
+	// 通过命名路由获取 URL 示例
+	homeURL, _ := router.Get("home").URL()
+	fmt.Println("homeURL: ", homeURL)
+	articleURL, _ := router.Get("articles.show").URL("id", "23")
+	fmt.Println("articleURL: ", articleURL)
 
-    http.ListenAndServe(":3000", router)
+	http.ListenAndServe(":3000", router)
 
 	//http.ListenAndServe 用以监听本地 3000 端口以提供服务，标准的 HTTP 端口是 80 端口
-	http.ListenAndServe(":3000", router)
+	http.ListenAndServe(":3000", removeTrailingSlash(router))
 }
