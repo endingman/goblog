da1146566939ba3048af7027074bca00abbf691e
endingman <631925002@qq.com>
2021-03-22 15:35:45 +0800

    读取请求数据

--- main.go
+++ main.go
@@ -46,7 +46,27 @@ func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "创建新的文章")
+	//Form：存储了 post、put 和 get 参数，在使用之前需要调用 ParseForm 方法。
+	//PostForm：存储了 post、put 参数，在使用之前需要调用 ParseForm 方法。
+	err := r.ParseForm()
+	if err != nil {
+		fmt.Fprint(w, "请提供正确的参数")
+		return
+	}
+
+	title := r.PostForm.Get("title")
+
+	fmt.Fprintf(w, "POST PostForm: %v <br>", r.PostForm)
+	fmt.Fprintf(w, "POST Form: %v <br>", r.Form)
+	fmt.Fprintf(w, "title 的值为: %v", title)
+
+	//如不想获取所有的请求内容，而是逐个获取的话，这也是比较常见的操作，
+	// 无需使用 r.ParseForm() 可直接使用 r.FormValue() 和 r.PostFormValue() 方法
+	fmt.Fprintf(w, "r.Form 中 title 的值为: %v <br>", r.FormValue("title"))
+	fmt.Fprintf(w, "r.PostForm 中 title 的值为: %v <br>", r.PostFormValue("title"))
+	fmt.Fprintf(w, "r.Form 中 test 的值为: %v <br>", r.FormValue("test"))
+	fmt.Fprintf(w, "r.PostForm 中 test 的值为: %v <br>", r.PostFormValue("test"))
+
 }
 
 func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
@@ -57,7 +77,8 @@ func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
     <title>创建文章 —— 我的技术博客</title>
 </head>
 <body>
-    <form action="%s" method="post">
+	
+    <form action="%s?test=data" method="post">
         <p><input type="text" name="title"></p>
         <p><textarea name="body" cols="30" rows="10"></textarea></p>
         <p><button type="submit">提交</button></p>
@@ -65,6 +86,9 @@ func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
 </body>
 </html>
 `
+	//<form action="%s?test=data" method="post">
+	//r.Form 比 r.PostForm 多了 URL 参数里的数据
+
 	storeURL, _ := router.Get("articles.store").URL()
 	fmt.Fprintf(w, html, storeURL)
 }
