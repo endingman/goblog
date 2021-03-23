191e61fd020d0b9fe31a0a2ad41eaa6bc4c0317f
endingman <631925002@qq.com>
2021-03-23 10:29:01 +0800

    连接数据库

--- main.go
+++ main.go
@@ -3,22 +3,26 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"html/template"
+	"log"
 	"net/http"
 	"net/url"
 	"strings"
+	"time"
 	"unicode/utf8"
 	/**
 	因为引入的是驱动，操作数据库时我们使用的是 sql 库里的方法，而不会具体使用到 go-sql-driver/mysql 包里的方法，
 	当有未使用的包被引入时，Go 编译器会停止编译。
 	为了让编译器能正常运行，需要使用 匿名导入 来加载。
-	 */
+	*/
 	_ "github.com/go-sql-driver/mysql"
 )
 
 //包级别的变量声明时不能使用 := 语法，修改为带关键词 var 的变量声明
 var router = mux.NewRouter().StrictSlash(true)
+var db *sql.DB
 
 type ArticlesFormData struct {
 	Title, Body string
@@ -185,7 +189,7 @@ func main() {
 	//localhost:3000/about/
 	//可以看到有 / 的链接会报 404 错误：
 	//Gorilla Mux 提供了一个 StrictSlash(value bool) 函数处理`/`问题
-
+	initDB()
 	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
 	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
 
@@ -209,7 +213,61 @@ func main() {
 	//http.ListenAndServe 用以监听本地 3000 端口以提供服务，标准的 HTTP 端口是 80 端口
 	http.ListenAndServe(":3000", removeTrailingSlash(router))
 }
-func init() {
-	sql.Register("mysql", &MySQLDriver{})
+
+func initDB() {
+	var err error
+
+	/**
+	DSN 全称为 Data Source Name，表示 数据源信息，用于定义如何连接数据库。
+	不同数据库的 DSN 格式是不同的，这取决于数据库驱动的实现，
+	下面是 go-sql-driver/sql 的 DSN 格式，如下所示：
+	//[用户名[:密码]@][协议(数据库服务器地址)]]/数据库名称?参数列表
+	[username[:password]@][protocol[(address)]]/dbname[?param1=value1&...&paramN=valueN]
+	*/
+
+	config := mysql.Config{
+		User:                 "homestead",
+		Passwd:               "secret",
+		Addr:                 "127.0.0.1:33060",
+		Net:                  "tcp",
+		DBName:               "goblog",
+		AllowNativePasswords: true,
+	}
+
+	// 准备数据库连接池
+	//config.FormatDSN() homestead:secret@tcp(127.0.0.1:33060)/goblog?checkConnLiveness=false&maxAllowedPacket=0
+	//func Open(driverName, dataSourceName string) (*sql.DB, error)
+	db, err = sql.Open("mysql", config.FormatDSN())
+
+	checkError(err)
+
+	// 设置最大连接数
+	//实验表明，在高并发的情况下，将值设为大于 10，可以获得比设置为 1 接近六倍的性能提升。
+	// 而设置为 10 跟设置为 0（也就是无限制），在高并发的情况下，性能差距不明显。
+	//需要考虑的是不要超出数据库系统设置的最大连接数。
+	//show variables like 'max_connections';
+	db.SetMaxOpenConns(25)
+	// 设置最大空闲连接数
+	/**
+	设置连接池最大空闲数据库连接数，<= 0 表示不设置空闲连接数，默认为 2。
+
+	实验表明，在高并发的情况下，将值设为大于 0，可以获得比设置为 0 超过 20 倍的性能提升。
+
+	这是因为设置为 0 的情况下，每一个 SQL 连接执行任务以后就销毁掉了，执行新任务时又需要重新建立连接。
+
+	很明显，重新建立连接是很消耗资源的一个动作。
+	*/
+	db.SetMaxIdleConns(25)
+	// 设置每个链接的过期时间
+	db.SetConnMaxLifetime(5 * time.Minute)
+
+	// 尝试连接，失败会报错
+	err = db.Ping()
+	checkError(err)
 }
 
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
