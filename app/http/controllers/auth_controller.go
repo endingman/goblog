f1a2653ed62ae4eb140452a974ba094cbdc6225d
endingman <631925002@qq.com>
2021-04-07 19:40:25 +0800

    注册表单验证

--- app/http/controllers/auth_controller.go
+++ app/http/controllers/auth_controller.go
@@ -1,13 +1,22 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"goblog/app/models/user"
+	"goblog/app/requests"
 	"goblog/pkg/view"
 	"net/http"
 )
 
 type AuthController struct {
+}
+
+type userForm struct {
+	Name            string `valid:"name"`
+	Email           string `valid:"email"`
+	Password        string `valid:"password"`
+	PasswordConfirm string `valid:"password_confirm"`
 }
 
 func (*AuthController) Register(w http.ResponseWriter, r *http.Request) {
@@ -16,26 +25,31 @@ func (*AuthController) Register(w http.ResponseWriter, r *http.Request) {
 
 // DoRegister 处理注册逻辑
 func (*AuthController) DoRegister(w http.ResponseWriter, r *http.Request) {
-
-	// 0. 初始化变量
-	name := r.PostFormValue("name")
-	email := r.PostFormValue("email")
-	password := r.PostFormValue("password")
-
-	// 1. 表单验证
-	// 2. 验证通过 —— 入库，并跳转到首页
+	// 1. 初始化数据
 	_user := user.User{
-		Name:     name,
-		Email:    email,
-		Password: password,
+		Name:            r.PostFormValue("name"),
+		Email:           r.PostFormValue("email"),
+		Password:        r.PostFormValue("password"),
+		PasswordConfirm: r.PostFormValue("password_confirm"),
 	}
-	_user.Create()
 
-	if _user.ID > 0 {
-		fmt.Fprint(w, "插入成功，ID 为"+_user.GetStringID())
+	// 2. 表单规则
+	errs := requests.ValidateRegistrationForm(_user)
+
+	if len(errs) > 0 {
+		// 3. 有错误发生，打印数据
+		data, _ := json.MarshalIndent(errs, "", "  ")
+		fmt.Fprint(w, string(data))
 	} else {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprint(w, "创建用户失败，请联系管理员")
+		// 4. 验证成功，创建数据
+		_user.Create()
+
+		if _user.ID > 0 {
+			http.Redirect(w, r, "/", http.StatusFound)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "注册失败，请联系管理员")
+		}
 	}
 
 	// 3. 表单不通过 —— 重新显示表单
--- app/models/user/user.go
+++ app/models/user/user.go
@@ -4,8 +4,11 @@ import "goblog/app/models"
 
 type User struct {
 	models.BaseModel
+	//GORM 默认会将键小写化作为字段名称，column 项可去除，另外默认是允许 NULL 的，故 default:NULL 项也可去除
+	Name     string `gorm:"type:varchar(255);not null;unique" valid:"name"`
+	Email    string `gorm:"type:varchar(255);unique;" valid:"email"`
+	Password string `gorm:"type:varchar(255)" valid:"password"`
 
-	Name     string `gorm:"column:name;type:varchar(255);not null;unique"`
-	Email    string `gorm:"column:email;type:varchar(255);default:NULL;unique;"`
-	Password string `gorm:"column:password;type:varchar(255)"`
+	// gorm:"-" —— 设置 GORM 在读写时略过此字段，仅用于表单验证
+	PasswordConfirm string `gorm:"-" valid:"password_confirm"`
 }
